c9f0b0d0deff1b97282190204640a801d4f5a72e
Yago Alexandre <yago.afonso@tcema.tc.br>
2023-06-15 11:15:29 -0300

    Obtendo usuario por ID

--- src/main/java/com/goltaraya/workshopmongo/services/UserService.java
+++ src/main/java/com/goltaraya/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.goltaraya.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.goltaraya.workshopmongo.domain.User;
 import com.goltaraya.workshopmongo.repositories.UserRepository;
+import com.goltaraya.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,12 @@ public class UserService {
 		List<User> list = repository.findAll();
 		return list;
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
+
+
+
