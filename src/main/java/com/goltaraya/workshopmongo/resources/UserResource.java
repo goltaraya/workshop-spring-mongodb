ace02a001ecae5c198861c7c4e0220fcd46f1b33
Yago Alexandre <goltarayago@gmail.com>
2023-06-18 14:43:28 -0300

    Endpoint para retornar posts de um determinado usuario

--- src/main/java/com/goltaraya/workshopmongo/resources/UserResource.java
+++ src/main/java/com/goltaraya/workshopmongo/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.goltaraya.workshopmongo.domain.Post;
 import com.goltaraya.workshopmongo.domain.User;
 import com.goltaraya.workshopmongo.dto.UserDTO;
 import com.goltaraya.workshopmongo.services.UserService;
@@ -61,4 +62,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@GetMapping(value = "{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
